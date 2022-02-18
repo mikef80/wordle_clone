4c0215ab92398a6a385d46b1e22d3fe6a4a4c4aa
Mike <mike@mike-francis.org>
2022-02-18 14:14:42 +0000

    currently at 36 mins in tutorial

---
+++ Youtube/app.js
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', () => {
+    createSquares();
+
+    let availableSpace = 1;
+    const guessedWords = [[]];
+    const keys = document.querySelectorAll('.keyboard-row button');
+
+    let word = 'dairy';
+    let guessedWordCount = 0;
+    
+    function getCurrentWordArr() {
+        const numberofGuessedWords = guessedWords.length;
+        return guessedWords[numberofGuessedWords - 1];
+    }
+    
+    function updateGuessedWords(letter) {
+        const currentWordArr = getCurrentWordArr();
+        
+        if (currentWordArr && currentWordArr.length < 5) {
+            currentWordArr.push(letter);
+            
+            const availableSpaceEl = document.getElementById(String(availableSpace));
+            availableSpace += 1;
+
+            availableSpaceEl.textContent = letter;
+        }
+    }
+
+    function getTileColour(letter, index) {
+        const isCorrectLetter = word.includes(letter);
+
+        if (!isCorrectLetter) {
+            return "rgb(58, 58, 60)";
+        }
+
+        const letterInThatPosition = word.charAt(index);
+        const isCorrectPosition = letter === letterInThatPosition;
+
+        if (isCorrectPosition) {
+            return "rgb(83, 141, 78)"
+        }
+
+        return "rgb(181, 159, 59)";
+    }
+
+    function handleSubmitWord(params) {
+        const currentWordArr = getCurrentWordArr();
+        if(currentWordArr.length !== 5) {
+            window.alert('Word must be 5 letters');
+        }
+
+        const currentWord = currentWordArr.join('');
+
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200;
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColour = getTileColour(letter, index);
+
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId);
+                letterEl.classList.add('animate__flipInX');
+                letterEl.style = `background-color:${tileColour}; border-color:${tileColour}`;
+
+            }, interval * index);
+        })
+
+        guessedWordCount += 1;
+
+        if (currentWord === word) {
+            window.alert('Congratulations!');
+        }
+
+        if(guessedWords.length === 6) {
+            window.alert(`Sorry, you have no more guesses! The word is ${word}`);
+        }
+
+        guessedWords.push([]);
+    }
+
+
+    
+    function createSquares() {
+        const gameBoard = document.getElementById('board');
+        
+        for (let index = 0; index < 30; index++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.classList.add('animate__animated');
+            square.setAttribute('id', index + 1);
+            gameBoard.appendChild(square);
+        }
+    }
+    
+
+
+
+
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({target}) => {
+            const letter = target.getAttribute('data-key');
+
+            if (letter === 'enter') {
+                handleSubmitWord();
+                return;
+            }
+
+
+
+            console.log(letter);
+            updateGuessedWords(letter);
+        }
+    }
+
+
+})
