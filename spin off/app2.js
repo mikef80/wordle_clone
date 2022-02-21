9c3ca5bf2263d1cc5001f71723f8ff52b6e0cb2d
Mike Francis <mfrancis@clever-touch.com>
2022-02-21 14:14:53 +0000

    updates

--- spin off/app2.js
+++ spin off/app2.js
@@ -3,6 +3,7 @@
 // THIS MAKES THE GAME LOAD ON PAGE LOAD
 document.addEventListener('DOMContentLoaded', () => {
     let answer;
+    let outputAnswer;
     
     buildGrid();
     setSwatch();
@@ -10,7 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const keys = document.querySelectorAll('.keyboard-row button');
     const guessedRGB = [[]];
     let currentSquareNo = 1;
+    let guessCount = 0;
     
+    
+    function numConv(n) {
+      return Number(n);
+    }
     
     function setSwatch() {
         let totalColors = 256;
@@ -19,8 +25,18 @@ document.addEventListener('DOMContentLoaded', () => {
         let g = (Math.floor(Math.random() * totalColors)).toString().padStart(3, '0');
         let b = (Math.floor(Math.random() * totalColors)).toString().padStart(3, '0');
 
-        answer = Number(r + g + b);
+        console.log(r);
+        console.log(g);
+        console.log(b);
+
+        outputAnswer = (`rgb(${r},${g},${b})`)
+
+
+        answer = (r + g + b);
         console.log(answer);
+        let answer2 = Array.from(String(answer), numConv); 
+
+        console.log(answer2);
         
         const colour = `${r}, ${g}, ${b}`;
         const swatch = document.querySelector('.colour');
@@ -28,30 +44,120 @@ document.addEventListener('DOMContentLoaded', () => {
         swatch.style.backgroundColor = `rgb(${colour})`;
     }
 
+    // getCurrentGuessArray
+    function getCurrentGuessArr() {
+      const numbersofGuesses = guessedRGB.length;
+      return guessedRGB[numbersofGuesses - 1];
+    }
+
+    // updateGuesses
+    function updateGuesses(digit) {
+      const currentGuessArr = getCurrentGuessArr();
+
+      if(currentGuessArr && currentGuessArr.length < 9) {
+          currentGuessArr.push(digit);
+
+          const availableSpaceEl = document.getElementById(String(currentSquareNo));
+          currentSquareNo += 1;
+
+          availableSpaceEl.textContent = digit;
+      }
+    }
+
+    // getTileColour
+    function getTileColour(digit, index) {
+      console.log(answer);
+      const isCorrectDigit = answer.includes(digit);
+
+      if(!isCorrectDigit) {
+        return "rgb(58, 58, 60)";
+      }
+
+      const digitInThatPosition = answer.charAt(index);
+      const isCorrectPosition = digit === digitInThatPosition;
+
+      if(isCorrectPosition) {
+        return "rgb(83, 141, 78)";
+      }
+
+      return "rgb(181, 159, 59)";
+    }
+
+    // handleSubmitGuess
+    function handleSubmitGuess() {
+      const currentGuessArr = getCurrentGuessArr();
+
+      if (currentGuessArr.length !== 9) {
+        window.alert('Guess must be 9 digits long!');
+      }
+
+      const currentGuess = currentGuessArr.join('');
+    
+
+      const firstDigitId = guessCount * 9 + 1;
+      const interval = 200;
+
+      currentGuessArr.forEach((digit, index) => {
+        setTimeout(() => {
+          const tileColour = getTileColour(digit, index);
+          const digitId = firstDigitId + index;
+          const digitEl = document.getElementById(digitId);
+          digitEl.classList.add('animate__flipInX');
+          // digitEl.style = `background-color:${tileColour}; border-color:${tileColour}`;
+          digitEl.style = `background-color:${tileColour};`;
+        }, interval * index);
+      })
+
+      guessCount += 1;
+
+      if (currentGuess === answer) {
+        window.alert('Congratulations!');
+        return;
+      }
+
+      if (guessedRGB.length === 3) {
+        window.alert(`Sorry, you have no more guesses! The answer is ${outputAnswer}`);
+        document.getElementById('test').textContent = outputAnswer;
+        return
+      }
+
+      guessedRGB.push([]);
+
+    }
 
 
-    // console.log(keys);
+    // handleDeleteDigit
+    function handleDeleteDigit() {
+      const currentGuessArr = getCurrentGuessArr();
+      const removedDigit = currentGuessArr.pop();
+
+      guessedRGB[guessedRGB.length - 1] = currentGuessArr;
+
+      const lastDigitEl = document.getElementById(String(currentSquareNo - 1));
+
+      lastDigitEl.textContent = '';
+      currentSquareNo = currentSquareNo - 1;
+    }
+
 
     // BUILD GAME GRID
     function buildGrid() {
         
         // sets number of rows and columns
-        const rows = 1;
-        const cols = 3;
+        const rows = 3;
+        // const cols = 3;
+        const cols = 9;
         // const cols = 1;
 
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < cols; j++) {
-                // console.log(`${i}, ${j}`);
 
                 let square = document.createElement('div');
-                // square.textContent = "x";
-                // square.textContent = (j + 1) % 3;
                 let board = document.querySelector('.board');
                 board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
 
-                square.classList.add('square','container');
-                square.id = "sq" + ((i * cols) + (j + 1));
+                square.classList.add('square','container','animate__animated');
+                square.id = (i * cols) + (j + 1);
                 board.appendChild(square);
 
             }
@@ -61,42 +167,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // INPUT NUMBERS INTO GRID
     keys.forEach(key => {
         key.onclick = ({target}) => {
-            const letter = target.getAttribute('data-key');
+            const number = target.getAttribute('data-key');
                         
-            if (letter === 'enter') {
+            if (number === 'enter') {
                 handleSubmitGuess();
                 return;
             }
             
-            if (letter === 'delete') {
-                handleDeleteNumber();
+            if (number === 'delete') {
+                handleDeleteDigit();
                 return;
             }
 
-            console.log(letter);
-            updateGuessNumbers(number);
+            console.log(number);
+            updateGuesses(number);
             
         }
     })
 })
-
-
-/* console.log(letter);
-// console.log(`${currentSquareNo}`);
-// let currentSq = document.querySelector("#" + currentSquareNo);
-let currentSq = document.querySelector("#sq" + currentSquareNo);
-console.log(currentSq.id);
-// let currentSq = document.getElementById('1'); */
-
-
-
-/* if (currentSq.innerHTML.length === 3) {
-    console.log('too long');
-    currentSquareNo++;
-    currentSq = document.querySelector("#sq" + (currentSquareNo));
-    currentSq.innerHTML += letter;
-} else if (letter === 'delete') {
-    
-} else {
-    currentSq.innerHTML += letter;
-} */
