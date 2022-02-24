9285a461b42cd52493a659e07b5bdd6d8daecbd0
Mike <mike@mike-francis.org>
2022-02-24 12:54:10 +0000

    grid ids sorted hopefully!

--- test grids/app.js
+++ test grids/app.js
@@ -1,55 +1,70 @@
-let cols = 3;
-let rows = 3;
+/* let cols = 3;
+let rows = 4; */
 
 // TEST ONE
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
 
-        let square = document.createElement('div');
-        let gridOne = document.querySelector('.test1');
-        gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+document.addEventListener('DOMContentLoaded', () => {
+    // buildGrid(1,3);
+    buildGrid(3,3);
 
-        square.classList.add('square');
-        // square.id = (i * cols) + (j + 1);
-        square.id = (i) + (j + 1);
-        square.textContent = `${square.id} (${i}, ${j})`;
-        // square.textContent = `i:${i}, j:${j}`;
-        gridOne.appendChild(square);
-
+    function buildGrid(rows,cols) {
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+    
+                let square = document.createElement('div');
+                let gridOne = document.querySelector('.test1');
+                gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    
+                square.classList.add('square');
+                // square.id = (i * cols) + (j + 1);
+                // square.id = (i * (cols * 3)) + (j + 1);
+                square.id = (i * (cols * 3)) + (j + ((0 * cols) + 1));
+                square.textContent = `${square.id}`;
+                // square.textContent = `${square.id} (${i}, ${j})`;
+                // square.textContent = `i:${i}, j:${j}`;
+                gridOne.appendChild(square);
+    
+            }
+        }
+    
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+    
+                let square = document.createElement('div');
+                let gridOne = document.querySelector('.test2');
+                gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    
+                square.classList.add('square');
+                // square.id = (i * cols) + (j + 1);
+                // square.id = (i * (cols * 3)) + (j + 4);
+                square.id = (i * (cols * 3)) + (j + ((1 * cols) + 1));
+                square.textContent = `${square.id}`;
+                // square.textContent = `${square.id} (${i}, ${j})`;
+                // square.textContent = `i:${i}, j:${j}`;
+                gridOne.appendChild(square);
+    
+            }
+        }
+    
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+    
+                let square = document.createElement('div');
+                let gridOne = document.querySelector('.test3');
+                gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    
+                square.classList.add('square');
+                // square.id = (i * cols) + (j + 1);
+                // square.id = (i * (cols * 3)) + (j + 7);
+                square.id = (i * (cols * 3)) + (j + ((2 * cols) + 1));
+                square.textContent = `${square.id}`;
+                // square.textContent = `${square.id} (${i}, ${j})`;
+                // square.textContent = `i:${i}, j:${j}`;
+                gridOne.appendChild(square);
+    
+            }
+        }
     }
-}
+})
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-
-        let square = document.createElement('div');
-        let gridOne = document.querySelector('.test2');
-        gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-
-        square.classList.add('square');
-        // square.id = (i * cols) + (j + 1);
-        square.id = (i) + (j + 4);
-        square.textContent = `${square.id} (${i}, ${j})`;
-        // square.textContent = `i:${i}, j:${j}`;
-        gridOne.appendChild(square);
-
-    }
-}
-
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-
-        let square = document.createElement('div');
-        let gridOne = document.querySelector('.test3');
-        gridOne.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-
-        square.classList.add('square');
-        // square.id = (i * cols) + (j + 1);
-        square.id = (i) + (j + 7);
-        square.textContent = `${square.id} (${i}, ${j})`;
-        // square.textContent = `i:${i}, j:${j}`;
-        gridOne.appendChild(square);
-
-    }
-}
